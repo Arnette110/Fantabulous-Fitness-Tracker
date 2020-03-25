90b0b492636a5f2e9c274400a88eddac539e676f
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-03-25 13:35:52 -0600

    changed name of database and enabled use path for routes

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitness', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
 })
 
-// routes
-// app.use(require('./routes/api.js'))
+//routes
+app.use(require('./routes/api.js'))
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`))
