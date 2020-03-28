e490081c05028fa13a23533f8da07a3df771cc9d
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-03-28 14:08:19 -0600

    changed local port because of cache issues

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -14,6 +14,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
 })
 
 //routes
-app.use(require('./routes/api.js'))
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`))
